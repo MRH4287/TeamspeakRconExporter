961746e9242fe1a1f8d6a9c86f885242eb78888d
Manuel Holzhauser <Manuel@mrh-development.de>
2020-03-21 15:14:04 +0100

    Add ChannelName as Metric-Label

--- TeamspeakRconExporter/Collectors/ClientsCollector.cs
+++ TeamspeakRconExporter/Collectors/ClientsCollector.cs
@@ -13,12 +13,12 @@ namespace TeamspeakRconExporter.Collectors
         #region Metrics
 
         private readonly Gauge _clientCount = Metrics.CreateGauge(Consts.GetMetricName("clientCount"), "The number of Clients currently on the Server");
-        private readonly Gauge _clients = Metrics.CreateGauge(Consts.GetMetricName("clients"), "The currently connected Clients", "NickName");
+        private readonly Gauge _clients = Metrics.CreateGauge(Consts.GetMetricName("clients"), "The currently connected Clients", "NickName", "ChannelName");
 
         #endregion
 
         private readonly Connection _connection;
-        private List<string> _lastSeenClients = new List<string>();
+        private List<(string, string)> _lastSeenClients = new List<(string, string)>();
 
 
         public ClientsCollector(Connection connection)
@@ -29,26 +29,30 @@ namespace TeamspeakRconExporter.Collectors
         public async Task Collect()
         {
             var allClients = await _connection.GetClients().ConfigureAwait(false);
+            var channels = await _connection.GetChannels().ConfigureAwait(false);
             var clients = allClients.Where(c => c.Type == TeamSpeak3QueryApi.Net.Specialized.ClientType.FullClient).ToList();
+            var channelMap = channels.ToDictionary(k => k.Id);
 
             _clientCount.Set(clients.Count);
 
-            var clientHash = new HashSet<string>();
+            var lastSeenData = new List<(string, string)>();
             foreach (var client in clients)
             {
-                _clients.WithLabels(client.NickName).Set(1);
-                clientHash.Add(client.NickName);
+                var channelName = channelMap.TryGetValue(client.ChannelId, out var channel) ? channel.Name : "";
+
+                _clients.WithLabels(client.NickName, channelName).Set(1);
+                lastSeenData.Add((client.NickName, channelName));
             }
 
             foreach (var lastSeen in _lastSeenClients)
             {
-                if (!clientHash.Contains(lastSeen))
+                if (!lastSeenData.Any(d => d.Item1 == lastSeen.Item1 && d.Item2 == lastSeen.Item2))
                 {
-                    _clientCount.WithLabels(lastSeen).Set(0);
+                    _clients.WithLabels(lastSeen.Item1, lastSeen.Item2).Set(0);
                 }
             }
 
-            _lastSeenClients = clientHash.ToList();
+            _lastSeenClients = lastSeenData;
 
         }
     }
